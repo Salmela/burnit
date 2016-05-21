2d9de9e67528ac3603bed8dca963ba61b59887de
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-21 20:01:09 +0300

    Add simple chart class.

--- test.rb
+++ test.rb
@@ -1,3 +1,22 @@
 require "net/http"
 
-puts "hello"
+class Chart
+	def initialize
+		@array = Array.new
+	end
+
+	def add_point(x, y)
+		@array.push([x, y])
+	end
+
+	def print
+		@array.each do |x|
+			puts x[0].to_s + ", " + x[1].to_s
+		end
+	end
+end
+
+chart = Chart.new
+chart.add_point(1, 2)
+chart.add_point(8, 3)
+chart.print
