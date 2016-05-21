c8765b6a5d951b6744a75fd77e67d5b98f2e04d6
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-21 20:13:53 +0300

    Implement litle svg builder.

--- test.rb
+++ test.rb
@@ -1,5 +1,22 @@
 require "net/http"
 
+class SvgBuilder
+	def initialize(filename, w, h)
+		@w = w
+		@h = h
+		@file = File.new(filename, "w")
+		@file.puts "<svg>"
+	end
+
+	def addRect(x, y, w, h)
+		@file.puts "<rect x=\"#{x}\" y=\"#{y}\" width=\"#{w}\" height=\"#{h}\" />"
+	end
+
+	def close
+		@file.puts "</svg>"
+	end
+end
+
 class Chart
 	def initialize
 		@array = Array.new
@@ -14,9 +31,16 @@ class Chart
 			puts x[0].to_s + ", " + x[1].to_s
 		end
 	end
+
+	def export
+		svg = SvgBuilder.new("test.svg", 100, 80)
+		svg.addRect(1, 1, 98, 78)
+		svg.close
+	end
 end
 
 chart = Chart.new
 chart.add_point(1, 2)
 chart.add_point(8, 3)
 chart.print
+chart.export
