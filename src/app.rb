e52ecfadf5772dae748a1eee12a1ec462cc48c52
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-22 12:32:28 +0300

    Use erb views and create layout for the app.

--- src/app.rb
+++ src/app.rb
@@ -2,30 +2,32 @@ require 'sinatra'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+	set :root, Proc.new { File.join(File.dirname(__FILE__), "../") }
+
 	get '/' do
-		"Hello World!"
+		erb :index
 	end
 
 	get '/:user/' do
-		"User #{params['user']}!"
+		erb "User #{params['user']}!"
 	end
 
 	get '/:user/:repo/' do
-		"Repo #{params['user']}/#{params['repo']}!"
+		erb "Repo #{params['user']}/#{params['repo']}!"
 	end
 
 	get '/:user/:repo/burndown' do
-		"Burndown of #{params['user']}/#{params['repo']}!"
+		erb "Burndown of #{params['user']}/#{params['repo']}!"
 	end
 
 	get '/:user/:repo/user-stories' do
-		"User stories of #{params['user']}/#{params['repo']}!"
+		erb "User stories of #{params['user']}/#{params['repo']}!"
 	end
 
-	set :public_folder, File.dirname(__FILE__) + '/../static'
+	set :public_folder, settings.root + '/static'
 
 	not_found do
 		status 404
-		'not found'
+		erb 'not found'
 	end
 end
