6285f9ec763a52910b933f57c0dc0497444cf1f0
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2016-05-22 18:14:16 +0300

    Fetch the repo issues from github api.

--- src/repo.rb
+++ src/repo.rb
@@ -1,7 +1,44 @@
 require_relative 'github_api'
 require_relative 'test.rb'
 
+class GithubIssue2
+	def initialize(data)
+		@data = data
+		#@url = json['url']
+		#@title = json['title']
+		#@number = json['number']
+		#@labels = json['labels']
+	end
+
+	def name
+		@data['title']
+	end
+
+	def url
+		@data['html_url']
+	end
+
+	def size
+		return @size.to_s if @size
+		@size = 2
+		return @size.to_s
+	end
+end
+
 module Repo
+	def fetch_issues(github_api, user, repo)
+		issues = Array.new
+		uri = URI("https://api.github.com/repos/#{user}/#{repo}/issues")
+		json_issues = github_api.load(uri)
+
+		@issues = Array.new
+		json_issues.each do |data|
+			issues.push(GithubIssue2.new(data))
+		end
+
+		return issues
+	end
+
 	# this should be generated per milestone not by repo
 	def create_repo_burndown_svg
 		chart = Chart.new
@@ -16,7 +53,11 @@ module Repo
 	end
 
 	def create_repo_page
-		erb :repo_view
+		tasks = fetch_issues(GithubApi.get_default, params['user'], params['repo'])
+		erb :repo_view, :locals => {
+			:tasks => tasks,
+			:user => params['user'],
+			:repo => params['repo']}
 	end
 	def create_burndown_page
 		erb "Burndown of #{params['user']}/#{params['repo']}!"
